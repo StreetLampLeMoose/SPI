84514df46bfe052c54d8cb7673595a32f2efa978
StreetLampLeMoose <jwoswald1008@gmail.com>
2025-04-10 15:45:56 -0400

    got app to start with no errors

--- draw.js
+++ draw.js
@@ -1,9 +1,13 @@
 //this module will draw the game 
-import Player from "./player.js";
-import Enemy from "./enemy.js";
-import PlayerBullet from "./playerBullet.js";
-import EnemyBullet from "./enemyBullet.js";
-playerBullet = null; //placeholder
-const player = new Player(canvas, 5, playerBullet);
-let game = document.getElementById("game");
+import {Player} from "./player.js";
+//import Enemy from "./enemy.js";
+//import PlayerBullet from "./playerBullet.js";
+//import EnemyBullet from "./enemyBullet.js";
+
+export default function draw() {
+    let game = document.getElementById("game")
+    let playerBullet = 1; //placeholder
+    const player = new Player(game, 5, playerBullet);
+    
+    }
 //constructors for the enemy and other bullets go here TODO
