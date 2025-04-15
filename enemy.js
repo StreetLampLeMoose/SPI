21a3a2b18b88dd2f4d781bfc267503159873e3fe
StreetLampLeMoose <jwoswald1008@gmail.com>
2025-04-15 16:47:28 -0400

    stubs for enemy.js

--- enemy.js
+++ enemy.js
@@ -1,1 +1,18 @@
 //This module will control the behavior (position, gun firing, etc) of an enemy player
+export class Enemy {
+
+    constructor(canvas,velocity,enemyBullet,x,y){
+        this.x = x;
+        this.y= y;
+        this.canvas = canvas;
+        this.velocity = velocity;
+    }
+
+    move(){ //controls movement of the enemy
+
+    }
+
+    shoot(){ //controls how the enemy shoots
+
+    }
+}
