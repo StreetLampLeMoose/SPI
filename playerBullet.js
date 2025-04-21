1a44c16f81929d1d7f10658f4d68c665db410812
StreetLampLeMoose <jwoswald1008@gmail.com>
2025-04-21 15:11:52 -0400

    Started on playerBullet.js

--- playerBullet.js
+++ playerBullet.js
@@ -1,1 +1,14 @@
 //this module will control the behavior of the player bullets
+
+export default class PlayerBullet {
+    constructor(canvas, velocity, x,y, bulletColor){
+        this.canvas = canvas;
+        this.x = x;
+        this.y = y;
+        this.velocity = velocity;
+        this.bulletColor = bulletColor;
+        this.width = 5;
+        this.height = 10;
+    }
+
+}
