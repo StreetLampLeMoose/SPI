51fab77fc053faa7950e7a8f4e8f6fd44ec82831
StreetLampLeMoose <jwoswald1008@gmail.com>
2025-04-28 16:20:56 -0400

    started on a block of moving enemies

--- draw.js
+++ draw.js
@@ -6,12 +6,26 @@ import PlayerBulletController from "./playerBulletController.js";
 const enemyStartPositions = [
     { x: 50, y: 50 }
 ]
+const blockEnemyPositions = [
+    { x: 50, y: 50 },
+    { x: 100, y: 50 },
+    { x: 150, y: 50 },
+    { x: 200, y: 50 },
+    { x: 250, y: 50 },
+    { x: 300, y: 50 },
+    { x: 350, y: 50 },
+    { x: 400, y: 50 }
+]
+let enemyType = "continuous"; //type of enemy movement, can be continuous or block
+let enemyType2 = "block";
 const game = document.getElementById("game");
 const ctx = game.getContext("2d");
 const playerBulletController = new PlayerBulletController(game, "blue") //creates a new player bullet controller object
 const player = new Player(game, 5, playerBulletController); //creates new player object
-const enemies= new enemyController(game, enemyStartPositions); //creates a new enemy controller object
+const enemies= new enemyController(game, enemyStartPositions, enemyType);
+const blockEnemies = new enemyController(game, blockEnemyPositions, enemyType2); //creates a new enemy controller object
 const collisionHandler = new handleCollisions(player, enemies, playerBulletController);
+const blockEnemyCollisionHandler = new handleCollisions(player, blockEnemies, playerBulletController); //creates a new collision handler object
 
 export default function draw() {
     drawPlayer();
@@ -23,11 +37,9 @@ export default function draw() {
 };
      //checks for collisions between the enemies and player bullets
     
-    
-    
-    
     export function gameStart(){ //starts the game, draws initial player and enemy positions
-        enemies.enemiesStart(); //draws the enemies on the screen
+        enemies.enemiesStart();
+        blockEnemies.enemiesStart(); //draws the enemies on the screen
     };
 
      //creates a new handle collisions object
@@ -46,8 +58,12 @@ export default function draw() {
         const enemyImage = new Image(); //creates a new enemy image object
         enemyImage.src = "./img/red.png"; //loads the enemy image
         enemies.enemyMove(); //moves the enemies
+        blockEnemies.enemyMove(); //moves the block enemies
         enemyImage.onload = () => { //draws the enemy image when it is loaded
             enemies.enemies.forEach((enemy) => { //for each enemy, draw the enemy image at the new position
+                ctx.drawImage(enemyImage, enemy.x, enemy.y); //draws the enemy image at the new position
+            });
+            blockEnemies.enemies.forEach((enemy) => { //for each enemy, draw the enemy image at the new position
                 ctx.drawImage(enemyImage, enemy.x, enemy.y); //draws the enemy image at the new position
             });
         }
@@ -72,5 +88,9 @@ export default function draw() {
     function collisions(){
         collisionHandler.playerCollideWithEnemy(); //checks for collisions between the player and enemies
         collisionHandler.playerCollideWithEnemyBullet(); //checks for collisions between the player and enemy bullets
-        collisionHandler.enemyCollideWithPlayerBullet(); //checks for collisions between the enemies and player bullets
+        collisionHandler.enemyCollideWithPlayerBullet();
+        
+        blockEnemyCollisionHandler.playerCollideWithEnemy();
+        blockEnemyCollisionHandler.playerCollideWithEnemyBullet(); //checks for collisions between the player and block enemies
+        blockEnemyCollisionHandler.enemyCollideWithPlayerBullet(); //checks for collisions between the enemies and player bullets
     }
--- enemyController.js
+++ enemyController.js
@@ -1,12 +1,13 @@
 //This module will track the postitions of multiple enemies 
 import {Enemy} from "./enemy.js";
-//import {forEach} from "./utils.js"; //import forEach function from utils.js
+
 
 export class enemyController {
-    constructor(canvas, enemyPositions){
+    constructor(canvas, enemyPositions, enemyType){
         this.canvas = canvas;
         this.enemies = enemyPositions; //array of enemy positions
         this.enemyBullet = 1;
+        this.enemyType = enemyType; //type of enemy                 
     }
     
     enemiesStart(){ //sets the start position of the enemies
@@ -15,10 +16,21 @@ export class enemyController {
         });
     };
 
-    enemyMove(){ //move the enemies
-        this.enemies.forEach((enemy) => {
-            enemy.move(); //move the enemy
-        });
-    };
+    enemyMove(){ //continuous move left and right
+        if(this.enemyType == "continuous"){
+            this.enemies.forEach((enemy) => {
+                enemy.move(); //move the enemy
+            });    
+        }else if(this.enemyType =="block"){
+            this.enemyBlockMove();
+        }
+    
+}
 
+    enemyBlockMove(){
+        //Move a block of enemies
+        //if the far right side of the block is at the right wall, reset the move down timer, change the velocity sign, and move down
+        //if the far left side of the block is at the left wall, reset the move down timer, change velocty sign,  and move down
+        this.enemies.forEach((enemy) => {enemy.moveDown();}); //placeholder
+    }
 }
